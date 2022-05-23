c7ea279231077f61665ae9db69233e7721e2a6c2
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-05-23 09:50:13 -0400

    commit tree 99f7255b5749c861c77ab05aca6d3a0b906b7335

--- index.js
+++ index.js
@@ -11,6 +11,67 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+// const titleCased = tutorials.map(function (letter)) => {
+
+//   return tutorials
+// }
+
+//function declaration
+const titleCased =  tutorial.map(function (capLetter) {
+  return capLetter.LowerCase().split(' ').map(function(lyric) {
+    return (lyric.charAt(0).toUpperCase() + word.slice(1))
+  }) .join(' ')
+})
+
+
+//tutorials is never called
+
+// const titleCased = function (tutorials){
+//   return function firstLetterCaps(tutorials) {
+//     tutorials = tutorials.toLowerCase().split(' ').map(function(letter){
+//       return (letter.charAt(0).toUpperCase() + letter.slice(1))
+//     }) .join(' ')
+//   }
+//   return titleCased
+// }
+
+
+
+
+//return letter.toUpperCase
+
+
+// let tutorialsSplit = tutorials.split(' ')
+// tutorials[i].charAt(0).toUpperCase()
+// tutorials[i].slice(1)
+
+// function titleCase(tutorials) {
+//   tutorials = tutorials.toLowerCase();
+//   tutorials = tutorials.split(' ');
+//   for (let i = 0; i < tutorials.length; i++) {
+//     tutorials[i] = tutorials[i].charAt(0).toUpperCase() + tutorials[i].slice(1);
+//     return tutorials.join(' ')
+//   }
+//   return titleCase
+// }
+
+
+
+
+// const titleCased = tutorials() {
+//   return
+// }
+
+
+
+
+//Have to include TitleCased 
+// ReferenceError: titleCased is not defined
+
+// function loopAndCaps(tutorials) {
+//   const capTitles = [];
+//   for(let letter of tutorials) {
+//     capTitles.push(letter.toUpperCase)
+//   }
+//   return capTitles
+// }
